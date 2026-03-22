175b01c462310993c3d8ae53426b3b4ee1c18270
gmarotta-80 <gmarotta@assettoimpresa.com>
2026-03-22 23:04:17 +0100

    Fix permessi completi v6
    Fix permessi completi v6

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
     // URL del Google Apps Script Web App (da aggiornare dopo il deploy)
-    API_URL: 'https://script.google.com/macros/s/AKfycbySXJF00zGUWKBNPQAaUU0maOJCEVVOmagKlYmtmxVzyxNgAuDwvfr6_lJchRQRq03G/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbyvmhw_anKxVYfQs7y0rY5V-_Z7AJniSI3zniC3jXYSSr3qrDAg-IxMvCCrWYwCCH7B/exec',
 
     // Dimensione massima file (25MB)
     MAX_FILE_SIZE: 25 * 1024 * 1024,
